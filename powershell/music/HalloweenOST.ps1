c1f1db371e011fe6a0972f6ae8fc863f66fc7619
Taylor Hendricks <55804712+taylordhendricks@users.noreply.github.com>
2024-06-03 16:34:18 -0400

    Create HalloweenOST.ps1

---
+++ powershell/music/HalloweenOST.ps1
@@ -0,0 +1,12 @@
+for ($i = 1; $i -le 7; $i++) {
+    [System.Console]::Beep(1109,200);
+    [System.Console]::Beep(740,200);
+    [System.Console]::Beep(740,200);
+    [System.Console]::Beep(1109,200);
+    [System.Console]::Beep(740,200);
+    [System.Console]::Beep(740,200);
+    [System.Console]::Beep(1109,200);
+    [System.Console]::Beep(740,200);
+    [System.Console]::Beep(1175,200);
+    [System.Console]::Beep(740,200);
+}
